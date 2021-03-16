18b801fd7762c3174d40cdd09dad40e387d29a0a
todian-tornero <todanexus@gmail.com>
2021-03-16 22:05:58 +0300

    Создал класс Pressure, добавил обработчик нажатия на кнопку

--- app/src/main/java/com/example/pressure/MainActivity.java
+++ app/src/main/java/com/example/pressure/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.pressure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clickOnButtinOK();
+    }
+
+    private void clickOnButtinOK(){                                 //Нажатие на кнопку "Сохранить"
+        Button buttonOK = findViewById(R.id.buttonOK);
+        buttonOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                @SuppressLint("WrongViewCast") EditText up = findViewById(R.id.pressureViewUp);
+                @SuppressLint("WrongViewCast") EditText down = findViewById(R.id.pressureViewDown);
+                @SuppressLint("WrongViewCast") EditText pulse = findViewById(R.id.pulse);
+                String upValue = up.getText().toString();
+                String downValue = down.getText().toString();
+                String pulseValue = pulse.getText().toString();
+                Pressure pressure = new Pressure(Integer.parseInt(upValue), Integer.parseInt(downValue), Integer.parseInt(pulseValue));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/pressure/Pressure.java
@@ -0,0 +1,13 @@
+package com.example.pressure;
+
+public class Pressure {
+    int Up;
+    int Down;
+    int pulse;
+
+    public Pressure(int up, int down, int pulse) {
+        this.Up = up;
+        this.Down = down;
+        this.pulse = pulse;
+    }
+}
